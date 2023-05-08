0be755b19cf11fcbbe880a6b9f59881bf0b2933e
Maksym Ostapchuk <gadget.add@gmail.com>
2023-05-08 09:26:40 +0300

    remove Providers to index.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,3 @@
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-
-import { store, persistor } from 'redux/store';
-
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
@@ -11,17 +6,13 @@ import { AppTitle, SectionTitle, Wrapper } from './App.styled';
 
 export const App = () => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Wrapper>
-          <AppTitle>Phonebook</AppTitle>
-          <SectionTitle>Add new contact</SectionTitle>
-          <ContactForm />
-          <SectionTitle>Contacts</SectionTitle>
-          <Filter />
-          <ContactList />
-        </Wrapper>
-      </PersistGate>
-    </Provider>
+    <Wrapper>
+      <AppTitle>Phonebook</AppTitle>
+      <SectionTitle>Add new contact</SectionTitle>
+      <ContactForm />
+      <SectionTitle>Contacts</SectionTitle>
+      <Filter />
+      <ContactList />
+    </Wrapper>
   );
 };
